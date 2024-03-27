0059ff150035c4cfc97f2dfbd3e327dfe5cf5e02
NicoHelemon <user5@indy-51.icsil1.epfl.ch>
2024-03-27 11:45:00 +0000

    cluster results

--- condor_exec.sh
+++ condor_exec.sh
@@ -3,4 +3,4 @@
 # conda deactivate
 # conda init
 # conda activate myenv
-python3 /home/indy-stg3/user5/MAproject/testing.py $1 $2 $3 $4 $5 $6 $7 $8 $9 $10 $11 $12 $13 $14 $15 $16
+python3 /home/indy-stg3/user5/MAproject/testing.py $1 $2 $3 $4 $5 $6 $7 $8 $9 $10 $11 $12
